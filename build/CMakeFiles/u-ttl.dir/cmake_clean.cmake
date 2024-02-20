e82a3c6a244f1f2649c7cbab743808ef6011683d
alexei-pav <loshapavlov@gmail.com>
2024-02-20 20:42:07 +0100

    migrate to cmake build system

---
+++ build/CMakeFiles/u-ttl.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/u-ttl.dir/src/main.cpp.o"
+  "CMakeFiles/u-ttl.dir/src/main.cpp.o.d"
+  "u-ttl"
+  "u-ttl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/u-ttl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
