18a39e74e350c40d50dcbacd5c19c10ec3c10146
alexei-pav <lohspavlov@gmail.com>
2024-05-25 21:08:05 +0200

    dow work

--- include/main.hpp
+++ include/main.hpp
@@ -3,4 +3,17 @@
 
 #include <iostream>
 
+#include <gtkmm/window.h>
+#include <gtkmm/button.h>
+
+class MainWindow: public Gtk::Window
+{
+    public:
+        MainWindow();           /* define class constructor */
+        virtual ~MainWindow();  /* define class destructor */
+
+    protected:
+        Gtk::Button btn1;       /* create button */
+};
+
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include "main.hpp"
+#include <gtkmm/application.h>
 
-int main(){
+int main(int argc, char *argv[])
+{
   std::cout << "привет, мир!" << std::endl;
+  auto app = Gtk::Application::create("org.gagagoose.uttl");  /* create app */
+
+  // Shows the window and returns when it is closed.
+  return app->make_window_and_run<MainWindow>(argc, argv);
 }
